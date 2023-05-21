d69578160201b2a347e3761e48740a88aa58754e
ninelore <ninelore@protonmail.com>
2023-05-21 17:43:19 +0200

    Added stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { AngularComponent } from './angular/angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "test", component: TestComponent},
+      {path: "angular", component: AngularComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
