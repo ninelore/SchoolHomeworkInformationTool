0a8dae64d2378120b037364ba03ca7215d32a06a
ninelore <ninelore@protonmail.com>
2023-05-23 23:04:11 +0200

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  _menuItems: Map<String, String>;
+
+  constructor() {
+    this._menuItems = new Map<String, String>();
+    // Menu items
+    this._menuItems.set("list", "Overview");
+    this._menuItems.set("test", "Test");
+  }
+
+  isLoggedIn(): boolean {
+    return true;
+  }
+
+  get menuItems(): Map<String, String> {
+    return this._menuItems;
+  }
 }
